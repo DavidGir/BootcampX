d31eb3869aae4c53b61302184d6aa9e6779441fc
David Giroux <DavidGiroux149@gmail.com>
2023-11-13 20:12:43 -0500

    add SQL query students w/ low avg completion time

---
+++ 3_queries_group-by/15_students_low_average_completion_time.sql
@@ -0,0 +1,10 @@
+-- Get the students who's average time it takes to complete an assignment is less than the average estimated time it takes to complete an assignment.
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+JOIN assignments ON assignment_submissions.assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY avg(assignment_submissions.duration) ASC;
