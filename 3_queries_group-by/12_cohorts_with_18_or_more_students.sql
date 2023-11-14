63c92e5245c31de1b6c52a3830ba51d78e74e75f
David Giroux <DavidGiroux149@gmail.com>
2023-11-13 19:14:00 -0500

    add SQL query for cohorts w/ more than 18 students

---
+++ 3_queries_group-by/12_cohorts_with_18_or_more_students.sql
@@ -0,0 +1,9 @@
+-- Get all cohorts with 18 or more students.
+
+SELECT cohorts.name as cohort_name, count(students.id) as student_count
+FROM students
+JOIN cohorts
+ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(students.id) >= 18
+ORDER BY student_count;
