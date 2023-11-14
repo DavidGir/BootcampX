cd959805a53ac0a834347a040364eb41a1242f5d
David Giroux <DavidGiroux149@gmail.com>
2023-11-13 23:54:06 -0500

    add SQL query for total ARs for a teacher

---
+++ 4_queries/16_teacher_assistance_requests.sql
@@ -0,0 +1,7 @@
+-- Get the total number of assistance_requests for a teacher.
+
+SELECT name, count(completed_at) as total_assistances
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
