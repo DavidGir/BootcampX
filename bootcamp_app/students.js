24192c50cddeb9f98fe8fb3547f3cbb04ecfd205
David Giroux <DavidGiroux149@gmail.com>
2023-11-16 00:38:47 -0500

    add catch to promise to handle errors

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -21,5 +21,5 @@ LIMIT ${limit};
     res.rows.forEach(user => {
       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
     });
-  });
+  }).catch(err => console.error('query error', err.stack));
 
