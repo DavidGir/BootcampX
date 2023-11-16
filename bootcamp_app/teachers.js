7c58e69688b9b4cd7b546e95ce36b7f7c23ab887
David Giroux <DavidGiroux149@gmail.com>
2023-11-16 00:52:03 -0500

    add node-postgres query for teacher AR per cohort

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohortName = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN students ON students.id = assistance_requests.student_id 
+JOIN cohorts ON cohorts.id = cohort_id
+JOIN teachers ON teachers.id = teacher_id
+WHERE cohorts.name LIKE '%${cohortName}%'
+ORDER BY teacher;
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
